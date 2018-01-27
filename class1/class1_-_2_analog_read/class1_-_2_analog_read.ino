12257c81e8aada335bc5c5a41a3268ed34125fce
Dr. Bleep <drbleep@bleeplabs.com>
2018-01-27 10:32:53 -0600

    Add files via upload

---
+++ class1/class1_-_2_analog_read/class1_-_2_analog_read.ino
@@ -0,0 +1,41 @@
+
+int led1_pin =  0;                // The pin for LED1
+int led1_state = 0;             // The state of LED1
+
+unsigned long  previous_time1 = 0;  // will store the last time LED1 was updated
+unsigned long current_time;         // will store the current "time"
+unsigned long interval1;              //the interval at which LED1 will change
+
+int pot1_value; //store the reading from the potentiomeer aka knob
+
+
+void setup() {
+  // set both LED pins as output:
+  pinMode(led1_pin, OUTPUT);
+
+}
+
+void loop()
+{
+
+  pot1_value = analogRead(A0); //Read the analog voltage at pin A0. Returns 0 for 0volts and 1023 for the max voltage (3.3V). This is due to it being a 10 bit ADC
+  interval1 = pot1_value;
+
+  current_time = millis();
+
+  if (current_time - previous_time1 > interval1) { //has "interval1" amount of time past since we last executed this code?
+    previous_time1 = current_time; //if so enter this part of code and remeber at what time we did that
+
+    // if the LED is off turn it on and vice versa:
+    if (led1_state == 0)
+      led1_state = 1;
+    else
+      led1_state = 0;
+
+    digitalWrite(led1_pin, led1_state);   // Write the value "led1_state" to led1_pin.
+  }
+
+}
+
+
+
---
+++ class1/class1_-_3_two_LEDs/class1_-_3_two_LEDs.ino
@@ -0,0 +1,62 @@
+
+int led1_pin =  0;                // The pin for LED1
+int led1_state = LOW;             // The state of LED1
+
+int led2_pin =  4;                // The LED2 pin
+int led2_state = 0;             // The LED2 state
+
+unsigned long  previous_time1 = 0;  // will store the last time LED1 was updated
+unsigned long  previous_time2 = 0;  // will store the last time LED2 was updated
+
+unsigned long current_time;        
+
+unsigned long interval1;                //the interval that LED1 will change
+unsigned long interval2;                //the interval that LED2 will change
+
+int pot1_value; //store the reading from the potentiomeer aka knob
+
+
+void setup() {
+  // set both LED pins as output:
+  pinMode(led1_pin, OUTPUT);
+  pinMode(led2_pin, OUTPUT);
+
+}
+
+void loop()
+{
+
+  pot1_value = analogRead(A0); //returns 0 to 1023;
+  interval1 = pot1_value;
+  interval2 = pot1_value / 2;
+
+  current_time = millis(); //call the function millis() to get the time in milliseconds that hve ellapsed since the device started
+
+  if (current_time - previous_time1 > interval1) { //has "interval1" amount of time past since we last executed this code?
+    previous_time1 = current_time;
+
+    // if the LED is off turn it on and vice versa:
+    if (led1_state == LOW)
+      led1_state = HIGH;
+    else
+      led1_state = LOW;
+
+    digitalWrite(led1_pin, led1_state);   // Write the value "led1_state" to "led1_pin.
+  }
+
+
+  if (current_time - previous_time2 > interval2) { //has "interval2" amount of time past since we last executed this code?
+    previous_time2 = current_time;
+
+    // if  LED2 is off turn it on and vice versa:
+    if (led2_state == 0)
+      led2_state = 1;
+    else
+      led2_state = 0;
+
+    digitalWrite(led2_pin, led2_state);   // Write the value "led2_state" to "led2_pin.
+
+  }
+
+}
+
